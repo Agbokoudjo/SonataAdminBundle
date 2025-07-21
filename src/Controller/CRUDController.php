9b8d0112526ec99f71cfc5d5621f965ea9e54069
INTERNATIONALES WEB SERVICES <franckagbokoudjo301@gmail.com>
2025-07-21 20:52:28 +0100

    Supprime l'ancien support de label_catalogue pour la prochaine version majeure

--- src/Controller/CRUDController.php
+++ src/Controller/CRUDController.php
@@ -39,7 +39,6 @@ use Symfony\Component\Form\FormView;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
